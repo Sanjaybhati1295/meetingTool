d4694df9bc229d653fca6574e869eed42d38b871
Sanjaybhati1295 <44433514+Sanjaybhati1295@users.noreply.github.com>
2025-03-24 20:14:07 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-
+const { Server } = require("socket.io"); // Import the Server class from socket.io
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
